52ed58d7ab60550cb7bd3531e0a5199f49f5c149
Daniel Morais Borges <daniel.morais@bumlai.com.br>
2021-05-12 21:29:59 -0400

    DbContext e DisciplinaController GetAll

---
+++ Controllers/DisciplinaController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAPiColaborativo.Model;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPiColaborativo.Controllers
+{
+    [Route("api/[controller]")]
+    public class DisciplinaController : ControllerBase
+    {
+        private readonly Disciplina _disciplinas;
+
+        public DisciplinaController()
+        {
+            _disciplinas = new Disciplina();
+
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_disciplinas.GetDisciplinas());
+        }
+    }
+}
--- Model/Disciplina.cs
+++ Model/Disciplina.cs
@@ -1,21 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using WebAPiColaborativo.Data;
+
 namespace WebAPiColaborativo.Model
 {
     public class Disciplina
     {
-        public Disciplina(int id, string nome, string curso, int cargaHoraria, string professor)
-        {
-            Id = id;
-            Nome = nome;
-            Curso = curso;
-            CargaHoraria = cargaHoraria;
-            Professor = professor;
-        }
-
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Curso { get; set; }
         public int CargaHoraria { get; set; }
         public string Professor { get; set; }
+
+        private readonly DbContext _dbContext;
+
+        public Disciplina()
+        {
+            _dbContext = new DbContext();
+        }
+
+        public List<Disciplina> GetDisciplinas()
+        {
+            var json = File.ReadAllText(_dbContext.CaminhoBanco());
+            var listaDisciplinas = JsonConvert.DeserializeObject<List<Disciplina>>(json);
+
+            return listaDisciplinas; 
+        }
+
+
     }
 }

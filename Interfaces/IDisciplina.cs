f6a3bbd5728449b0638c3acaab6956b1702bd0dc
Daniel Morais Borges <daniel@bumlai.com.br>
2021-05-13 16:23:13 -0400

    Teste Unitário Listagem de Disciplinas

--- Interfaces/IDisciplina.cs
+++ Interfaces/IDisciplina.cs
@@ -6,11 +6,11 @@ namespace WebAPiColaborativo.Interfaces
 {
     public interface IDisciplina
     {
-        public List<Disciplina> GetDisciplinas();
-        public bool AtualizarDbDisciplina(List<Disciplina> listaDisciplinas);
-        public Disciplina Inserir(Disciplina disciplica);
-        public Disciplina AtualizarDisciplina(int id, Disciplina disciplina);
-        public bool Deletar(int id);
+        List<Disciplina> GetDisciplinas();
+        bool AtualizarDbDisciplina(List<Disciplina> listaDisciplinas);
+        Disciplina Inserir(Disciplina disciplica);
+        Disciplina AtualizarDisciplina(int id, Disciplina disciplina);
+        bool Deletar(int id);
     }
 
 }

bc793a6c98cfe977d2c35b2d22e127cc908d0744
Daniel Morais Borges <daniel.morais@bumlai.com.br>
2021-05-12 20:56:48 -0400

    Primeira commit Classe Disciplina criada

---
+++ Model/Disciplina.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace WebAPiColaborativo.Model
+{
+    public class Disciplina
+    {
+        public Disciplina(int id, string nome, string curso, int cargaHoraria, string professor)
+        {
+            Id = id;
+            Nome = nome;
+            Curso = curso;
+            CargaHoraria = cargaHoraria;
+            Professor = professor;
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Curso { get; set; }
+        public int CargaHoraria { get; set; }
+        public string Professor { get; set; }
+    }
+}

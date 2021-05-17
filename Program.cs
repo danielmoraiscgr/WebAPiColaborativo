c89c73376534dfbbacf1dd26df277e4bd2f1327b
Travelmond <travelmond@gmail.com>
2021-05-17 19:56:42 -0400

    Commit para contribuir na Main

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-// Criado um comentário para o commit*/
+// Criado um comentário para o commit na main*/
 namespace WebAPiColaborativo
 {
     public class Program

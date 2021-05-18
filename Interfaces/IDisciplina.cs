25489b8da1c5b01af5598aa7073ceab675a5617a
Daniel Morais Borges <daniel@bumlai.com.br>
2021-05-18 18:12:03 -0400

    Atualização tabela de Professores

--- Interfaces/IDisciplina.cs
+++ Interfaces/IDisciplina.cs
@@ -7,9 +7,13 @@ namespace WebAPiColaborativo.Interfaces
     public interface IDisciplina
     {
         List<Disciplina> GetDisciplinas();
+
         bool AtualizarDbDisciplina(List<Disciplina> listaDisciplinas);
+
         Disciplina Inserir(Disciplina disciplica);
+
         Disciplina AtualizarDisciplina(int id, Disciplina disciplina);
+
         bool Deletar(int id);
     }
 

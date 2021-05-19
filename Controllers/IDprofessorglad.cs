12d81893103fc2473c7a9bfc93bd056cbe61baff
lucas targino <lucastargino99@hotmail.com>
2021-05-18 21:27:49 -0400

    commit

---
+++ Controllers/IDprofessorglad.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAPiColaborativo.Model;
+
+public class IDprofessorgrad
+{
+    public IDprofessorgrad()
+    {
+        public async Task<IActionResult> Update([FromBody] professor professor, int id, string graducao)
+        {
+            try
+            {
+                if (id != professor.Id)
+                {
+                    return null;
+                }
+            }   if (id != graducao.ToString)
+
+                return null;
+        }
